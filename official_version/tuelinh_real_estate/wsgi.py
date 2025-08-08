d64b5c1e9220506a241d9c5eeed0d7813b3a1641
PhamVanThai96 <tanngo.996@gmail.com>
2025-08-08 22:48:19 +0700

    version 5

--- official_version/tuelinh_real_estate/wsgi.py
+++ official_version/tuelinh_real_estate/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tuelinh_real_estate.settings')
 
 application = get_wsgi_application()
+app = application  # Add this line for Vercel
